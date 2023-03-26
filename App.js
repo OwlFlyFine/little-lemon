b8027d042977926cc748ab7b3bcf26f10151e02e
Piyapol Raengtip <owlflyfine@gmail.com>
2023-03-26 11:25:31 -0400

    feat: implement condition route

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import OnboardingScreen from "./screens/Onboarding";
+import HomeScreen from "./screens/Home";
+import ProfileScreen from "./screens/Profile";
+import SplashScreen from "./screens/Splash";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [state, setState] = useState(null);
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("onboard");
+      setState(jsonValue != null ? JSON.parse(jsonValue) : null);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const [fontsLoaded] = useFonts({
     "MarkaziText-Regular": require("./assets/fonts/MarkaziText-Regular.ttf"),
     "Karla-Regular": require("./assets/fonts/Karla-Regular.ttf"),
   });
+
+  if (isLoading) {
+    return <SplashScreen />;
+  }
 
   if (!fontsLoaded) {
     return null;
@@ -23,7 +49,14 @@ export default function App() {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        {state.isOnboardingCompleted ? (
+          <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
