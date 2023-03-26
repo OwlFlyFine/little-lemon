cea83bb5519d3a133dbd825c3b285827e756a9e9
Piyapol Raengtip <owlflyfine@gmail.com>
2023-03-26 07:15:10 -0400

    feat: implement custom fonts

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import OnboardingScreen from "./screens/Onboarding";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    "MarkaziText-Regular": require("./assets/fonts/MarkaziText-Regular.ttf"),
+    "Karla-Regular": require("./assets/fonts/Karla-Regular.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <OnboardingScreen />;
+}
