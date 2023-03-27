7830b003b1ac978bb1543678ea55dccf051bd957
Piyapol Raengtip <owlflyfine@gmail.com>
2023-03-27 13:55:16 -0400

    feat: only show profile screen

--- App.js
+++ App.js
@@ -51,7 +51,7 @@ export default function App() {
         }}>
         {state.isOnboardingCompleted ? (
           <>
-            <Stack.Screen name="Home" component={HomeScreen} />
+            {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
             <Stack.Screen name="Profile" component={ProfileScreen} />
           </>
         ) : (
