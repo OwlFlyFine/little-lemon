f24a0b5b055346811c04f95d05f71d284115e2a3
Piyapol Raengtip <owlflyfine@gmail.com>
2023-04-16 14:12:06 -0400

    refactor: code to insert table

--- utils/database.js
+++ utils/database.js
@@ -6,7 +6,7 @@ export async function createTable() {
     db.transaction(
       (tx) => {
         tx.executeSql(
-          "create table if not exists menuitems (id integer primary key not null, uuid text, title text, price text, category text);"
+          "create table if not exists menuitems (id integer primary key not null, name text, price text, description text, image text, category text);"
         );
       },
       reject,
@@ -25,15 +25,33 @@ export async function getMenuItems() {
   });
 }
 
-export function saveMenuItems(menuItems) {
-  db.transaction((tx) => {
-    tx.executeSql(
-      `insert into menuitems (name, price, description, image, category) values ${menuItems
-        .map(
-          (item) =>
-            `('${item.name}', '${item.price}', '${item.description}', '${item.image}', '${item.category}')`
-        )
-        .join(", ")}`
-    );
+export async function saveMenuItems(menuItems) {
+  return new Promise((resolve, reject) =>
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          `insert into menuitems (name, price, description, image, category) values ('${menuItems.name}', '${menuItems.price}', '${menuItems.description}', '${menuItems.image}', '${menuItems.category}')`
+        );
+      },
+      reject,
+      resolve
+    )
+  );
+}
+
+export async function filterByQueryAndCategories(query, activeCategories) {
+  //   console.log("query ", query);
+  const categoriesString = activeCategories.map((c) => `'${c}'`).join(", ");
+  //   console.log("category ", categoriesString);
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from menuitems where name like '%${query}%' and category in (${categoriesString})`,
+        [],
+        (_, { rows }) => {
+          resolve(rows._array);
+        }
+      );
+    });
   });
 }
